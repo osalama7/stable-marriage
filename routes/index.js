38ff0f8560901012d3a950385e985b97d891e3ec
Osama Salama <osama.salama@hotmail.co.uk>
2018-04-20 16:18:45 +0200

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Stable marriage my monkey to your pouch' });
+});
+
+module.exports = router;
